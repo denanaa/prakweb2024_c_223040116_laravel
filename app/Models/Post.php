6b28dee469b6d73f2ac59ba4ce4a71945abebbbf
napollux <denanaw30@gmail.com>
2024-10-28 23:33:13 +0700

    Video #9 Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,35 +1,13 @@
 <?php 
+
 namespace App\Models;
-use Illuminate\Support\Arr;
-class Post
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    public static function all(){
-        return [
-            [
-                'id'=> '1',
-                'slug' => 'judul-artikel-1',
-                'title' => 'Artikel 1',
-                'author' => 'Dena Astia',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi, sit ducimus vero, fugiat perspiciatis cumque sapiente ut placeat iusto beatae! Voluptate cupiditate optio provident ipsam numquam quam obcaecati architecto?'
-            ],
-            [
-                'id'=> '2',
-                'slug' => 'judul-artikel-2',
-                'title' => 'Artikel 2',
-                'author' => 'Dena Astia',
-                'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis enim ipsum laudantium expedita placeat labore autem excepturi inventore fugiat nisi quaerat, cum eaque tempore delectus perferendis vitae. Beatae, ea atque!'
-            ],
-        ];
-    }
-    public static function find($slug): array{
-        $post = Arr::first(Post::all(), fn ($post) => $post['slug'] == $slug);
-        if(!$post){
-            abort(404);
-        }
-        return $post;
-        // Arr::first(Post::all(), function($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // });
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
     
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,11 +6,7 @@
             <h2 class="mb-1 text-3xl -tracking-tight font-bold text-neutral-900">{{ $post ['title'] }}</h2>
         </a>
         <div class="text-base text-neutral-500">
-            <a href="#">{{ $post ['author'] }}</a> | 1 Januari 2024
+        <a href="#">{{ $post ['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="my-4 font-light">{{ Str::limit($post ['body'], 150 ) }}</p>
         <a href="/posts/{{ $post ['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-    </article>
-    @endforeach
-</x-layout>
-‎routes/web.php
