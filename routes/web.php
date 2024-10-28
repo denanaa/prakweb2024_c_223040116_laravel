ba9fc654323bffe04eeb1e1e549d3c31a1c606a4
napollux <denanaw30@gmail.com>
2024-10-28 22:03:13 +0700

    Video #4 Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,11 @@ Route::get('/about', function () {
     return view('about', ['nama' => 'Dena Astia']);
 });
 
+Route::get('/blog', function () {
+    return view('blog');
+});
 
+Route::get('/contact', function () {
+    return view('contact');
+});
 
