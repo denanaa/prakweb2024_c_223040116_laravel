67ee9bcec1486c5dce63f6b6ca7e3a6b13ad9e4a
napollux <denanaw30@gmail.com>
2024-10-28 23:53:38 +0700

    Video #14 N + Problem

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
-
+     // melakukan query dengan eager loading
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 Route::get('/posts/{post:slug}', function(Post $post) {
     // $post = Post::find($slug);;
@@ -23,13 +25,17 @@ Route::get('/posts/{post:slug}', function(Post $post) {
 });
 
 Route::get('/authors/{user:username}', function(User $user) {
+    // melakukan eager loading pada author dan category pada post yang dimiliki oleh user
+    // $posts = $user->posts->load('category', 'author');
     return view('posts', [
         'title' => count($user->posts) . ' Articles By ' . $user->name,
         'posts' => $user->posts
     ]);
 });
 Route::get('/categories/{category:slug}', function(Category $category)) {
+     // $posts = $category->posts->load('category', 'author');
     return view ('posts', ['title' => 'Article in :' . $category->name, 'posts' => $category->posts]);
+});
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
