363c48044fa59e915fc832f991497e4b04206225
napollux <denanaw30@gmail.com>
2024-10-28 23:04:53 +0700

    Video #5 Blade Component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">About</a>
-    <a href="/contact">About</a>
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li>
-            <a href="https://intagram.com/denaaws">Intagram</a>
-        </li>
-        <li>
-            <a href="https://x.com/napollux">X</a>
-        </li>
-        <li>
-            <a href="https://youtube.com/awbanana">You Tube</a>
-        </li>
-    </ul>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcome to my Blogsd</h3>
+</x-layout>
