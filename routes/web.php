64f95b28dcc51e44ffa4cc31d3cd7699e235fb2c
napollux <denanaw30@gmail.com>
2024-10-28 23:20:18 +0700

    Videp #7 Model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-v
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,47 +12,14 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' =>
-[
-    [
-        'id'=> '1',
-        'slug' => 'judul-artikel-1',
-        'title' => 'Artikel 1',
-        'author' => 'Dena Astia',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi, sit ducimus vero, fugiat perspiciatis cumque sapiente ut placeat iusto beatae! Voluptate cupiditate optio provident ipsam numquam quam obcaecati architecto?'
-    ],
-    [
-        'id'=> '2',
-        'slug' => 'judul-artikel-2',
-        'title' => 'Artikel 2',
-        'author' => 'Dena Astia',
-        'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis enim ipsum laudantium expedita placeat labore autem excepturi inventore fugiat nisi quaerat, cum eaque tempore delectus perferendis vitae. Beatae, ea atque!'
-    ],
-]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 
 });
 Route::get('/posts/{slug}', function($slug){
-    $posts = [
-        [
-            'id'=> '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Artikel 1',
-            'author' => 'Dena Astia',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi, sit ducimus vero, fugiat perspiciatis cumque sapiente ut placeat iusto beatae! Voluptate cupiditate optio provident ipsam numquam quam obcaecati architecto?'
-        ],
-        [
-            'id'=> '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Artikel 2',
-            'author' => 'Dena Astia',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis enim ipsum laudantium expedita placeat labore autem excepturi inventore fugiat nisi quaerat, cum eaque tempore delectus perferendis vitae. Beatae, ea atque!'
-        ],
-    ];
-    $post = Arr::first($posts, function($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
